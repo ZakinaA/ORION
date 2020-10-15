d4fc42287bd3dc26b937efb88cfeb6ed496bd645
ValentineD14 <72193729+ValentineD14@users.noreply.github.com>
2020-10-15 15:26:22 +0200

    Merge accueil > master (#2)
    * Modif nom du projet
    
    * Ajout de la page d'accueil (SANS le calendrier de ventes)
    
    * Ajout page d'accueil (Fichiers manquants + SANS calendrier)

--- src/java/servlets/ServletClient.java
+++ src/java/servlets/ServletClient.java
@@ -84,7 +84,7 @@ public class ServletClient extends HttpServlet {
         
        String url = request.getRequestURI();
        
-       if(url.equals("/EquidaWeb20/ServletClient/ajouterClient"))
+       if(url.equals("/Orion/ServletClient/ajouterClient"))
         {                   
             ArrayList<Pays> lesPays = PaysDAO.getLesPays(connection);
             request.setAttribute("pLesPays", lesPays);
